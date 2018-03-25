d34c187ffba24d5317cc9c0e1e11c8d42eb40ca1
Banjo Fox <drbanjofox@protonmail.com>
2018-03-25 12:28:03 -0400

    Add En-us language strings to struct for testing.

---
+++ src/aardwolf/lang-strings.rs
@@ -0,0 +1,61 @@
+let en_us = Lang {
+// multipurpose
+instanceTitle: String::from("Aardwolf Instance"),
+go_back: String::from("Go back"),
+submit: String::from("Submit"),
+commentPlaceholder: String::from("Add a comment..."),
+
+// asideSettings
+settings: String::from("Settings"),
+editProfile: String::from("Edit Profile"),
+preferences: String::from("Preferences"),
+muteWords: String::from("Muted Keywords"),
+muteUsers: String::from("Muted Users"),
+notifications: String::from("Notifications"),
+security: String::from("Security"),
+dataImport: String::from("Data Import"),
+dataExport: String::from("Data Export"),
+authApps: String::from("Authorized Apps"),
+authFollow: String::from("Authorized Followers"),
+invites: String::from("Invite People"),
+
+// asideShortcuts
+news: String::from("News Feed"),
+messages ="Messages"),
+shortcuts: String::from("Shortcuts"),
+calendar: String::from("Calendar"),
+groups: String::from("Groups"),
+lists: String::from("Lists"),
+photos: String::from("Photos"),
+favorites: String::from("Favorites"),
+weather: String::from("Weather"),
+create: String::from("Create"),
+newEvent: String::from("New Event"),
+newGroup: String::from("New Group"),
+
+// profileEdit
+profileTitle: String::from("Edit Profile"),
+profileSubTitle: String::from("Subtitle"),
+profileDisplayName: String::from("Display Name"),
+profileCharRemaining: String::from("characters left"),
+profileBio: String::from("About you"),
+profileAvatarUpload: String::from("Upload Avatar"),
+profileAvatarUploadBtnLabel: String::from("Choose a file…"),
+profileAvatarFileName: String::from("No file chosen"),
+profileAvatarSubText: String::from("PNG, GIF or JPG. At most 2MB. Will be downscaled to 120x120px"),
+profileHeaderUpload: String::from("Upload Avatar"),
+profileAvatarHeaderLabel: String::from("Choose a file…"),
+profileHeaderFileName: String::from("No file chosen"),
+profileHeaderSubText: String::from("PNG, GIF or JPG. At most 2MB. Will be downscaled to 700x335px"),
+
+// messaging
+enterPost: String::from("Press enter to awoo"),
+post: String::from("Awoo"),
+reply: String::from("Rewoo"),
+
+// footer
+terms: String::from("Terms of Service"),
+copyright: String::from("Copyright 2018"),
+github: String::from("Check us out on GitHub"),
+donate: String::from("Buy the team a coffee"),
+};
